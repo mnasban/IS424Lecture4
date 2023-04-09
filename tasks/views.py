3191fb210047eb7e8628f594875dea1b7763b597
KSUIS324 <mbinnasban@ksu.edu.sa>
2023-04-09 21:24:42 +0300

    add task 'handling form'

--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,36 @@
 from django.shortcuts import render
-tasks=["task 1","task 2","task 3"]
+from django import forms
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
+class NewTaskForm(forms.Form):
+     task=forms.CharField(label="New Task")
+
+#tasks=["task 1","task 2","task 3"]
 
 # Create your views here.
 def index(request):
+     if "tasks" not in request.session:
+          request.session["tasks"]=[]
+
      return render(request, "tasks/index.html",{
-          "tasks":tasks
+          "tasks":request.session["tasks"]
      })
 
 def add(request):
+     if request.method == "POST":
+          form=NewTaskForm(request.POST)
+
+          if form.is_valid():
+               task=form.cleaned_data["task"]
+               request.session["tasks"]+=[task]
+
+               return HttpResponseRedirect(reverse("tasks:index"))
+          else:
+               return render(request, "tasks/add.html" , {
+                    "form":form
+                    })
      return render(request, "tasks/add.html",{
-          "tasks":tasks
+          "form":NewTaskForm()
      })
+

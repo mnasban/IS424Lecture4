f8c81b5c8d023f70ecc1d3f4a5f8b585c65715d4
KSUIS324 <mbinnasban@ksu.edu.sa>
2023-04-09 09:11:24 +0300

    created and configured and changed tasks upto slide 52

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+tasks=["task 1","task 2","task 3"]
 
 # Create your views here.
 def index(request):
-     return render(request, "tasks/index.html")
+     return render(request, "tasks/index.html",{
+          "tasks":tasks
+     })
+
+def add(request):
+     return render(request, "tasks/add.html",{
+          "tasks":tasks
+     })

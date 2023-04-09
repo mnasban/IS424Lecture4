023076e1ef53701d1cd9ce5dcef1b897c09e32e1
KSUIS324 <mbinnasban@ksu.edu.sa>
2023-04-09 06:26:04 +0300

    correcting adding saad,fahad

--- LectureApp/urls.py
+++ LectureApp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
      path("", views.index, name="index"),
-     path("saad", views.index, name="index"),
-     path("fahad", views.index, name="index")
+     path("saad", views.saad, name="saad"),
+     path("fahad", views.fahad, name="fahad")
  ]

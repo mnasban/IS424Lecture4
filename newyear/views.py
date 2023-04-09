466e8368f5fdb80774eb46b893e00df6ebde882f
KSUIS324 <mbinnasban@ksu.edu.sa>
2023-04-09 07:58:11 +0300

    corrections to checking the new years

--- newyear/views.py
+++ newyear/views.py
@@ -5,6 +5,5 @@ import datetime
 def index(request):
      now=datetime.datetime.now()
      return render(request, "newyear/index.html", {
-          "newyear":True
+          "newyear":"newyear":now.month == 1 and now.day == 1
      })
-#"newyear":now.month == 1 and now.day == 1
